b744c74241ae74d559ba4561f611d822ee716e49
CFC-Tech-AI <pythoncfc@gmail.com>
2024-02-29 16:11:09 +0530

    commit

--- capAdmin_app/urls.py
+++ capAdmin_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from  .views import *
 
+app_name = "capAdmin_app"
+
 
 urlpatterns = [
     path('', admin_base, name='admin_base'),
+    path('profile/', profile, name='profile'),
+    path('profile_list/', profile_list, name='profile_list'),
 
 ]
--- capAdmin_app/views.py
+++ capAdmin_app/views.py
@@ -5,3 +5,14 @@ from django.shortcuts import render
 
 def admin_base(request):
     return render(request, 'admin_base.html')
+
+
+
+def profile(request):
+
+    return render(request, 'admin_panel/myprofile.html')
+
+
+def profile_list(request):
+    some_variable = range(1, 51)
+    return render(request, 'admin_panel/profile_list.html', {'some_variable': some_variable})
